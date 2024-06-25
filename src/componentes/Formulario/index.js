18e945098225bcbbd62841a27848f6d57da774c0
MatheusMelo-Sauter <mmelo@sauter.digital>
2024-06-25 14:24:06 -0300

    alteracoes

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,31 @@
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao  from '../Botao'
+import './Formulario.css'
+
+const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'Ux e Desing',
+        'Mobile', 
+        'Inovação e Gestão'
+    ]
+
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
+                <Botao texto="Criar Card"></Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
